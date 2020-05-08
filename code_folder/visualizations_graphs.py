3ea8cec9b30e9fc33ef35273396614456a7d5c9a
aapeebles <alison.peeblesmadigan@flatironschool.com>
2020-05-08 09:51:50 -0400

    updating images and removing neutering tables

--- code_folder/visualizations_graphs.py
+++ code_folder/visualizations_graphs.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 import matplotlib.cm as cm
+import numpy as np
 
 import seaborn as sns
 
@@ -125,6 +126,8 @@ def time_series_plot(dataset,title,xlab,ylab,file_name, y_tick_num):
         plt.close(fig)  
         pass
     
+        
+    
 """Same thing as above except with percent formatting"""    
 def time_series_plot_percent(dataset,title,xlab,ylab,file_name, y_tick_num):
         """This function:
@@ -216,3 +219,101 @@ def time_series_plot_percent(dataset,title,xlab,ylab,file_name, y_tick_num):
         plt.savefig(path)
         plt.close(fig)  
         pass 
+    
+    
+    
+    
+    
+def time_series_roll_plot(dataset,title,xlab,ylab,file_name, color, y_tick_num):
+        """This function:
+        - takes a pd.Series output from value_counts() and converts it to a pd.DataFrame
+        - formats the css and html to make it look nice
+        - renders the html
+        - converts the html to a PNG file
+        - crops the image
+        - saves the updated image file
+        
+        
+        Parameters
+        ----------
+        dataset : DataFrame
+            Output from a summary table created by using value_counts()
+        title : string
+            Title on graph
+        xlab : string
+        ylab : string
+        file_name : string
+            what you want the image file to be named
+
+        Returns
+        -------
+        file_name.png : saves image of matplotlib output"""
+        
+        
+    
+        # assign argument values to variables
+        file_name = file_name
+        title = title
+        xlab = xlab
+        ylab = ylab
+        data = dataset.copy()
+
+        # Create figure container
+        fig = plt.figure(figsize=(16, 10), dpi=80) 
+
+
+        # Creates one subplot within our figure and uses the classes fig and ax
+        fig, ax = plt.subplots(figsize=(16, 10), dpi= 80, facecolor='w', edgecolor='k')
+        
+        color=color
+        lin_style = ['-', '--', ':']
+        cm.rainbow(np.linspace(0,1,len(data.columns.tolist())))
+
+        # Loops through species catagory and adds them to the plot
+        for col_name, l in zip(data.columns.tolist(),lin_style):
+            ax.plot(data.index, col_name, linestyle=l, data=data, linewidth=4,c=color)
+
+        # Format the ticks
+
+        ## X Axis    
+        ### Specify variables I want to use
+        years = mdates.YearLocator()   # every year
+        months = mdates.MonthLocator(interval=3)  # every month
+        years_fmt = mdates.DateFormatter('%b-%Y')
+        mon_fmt = mdates.DateFormatter('%b')
+
+        ax.xaxis.set_major_locator(years)
+        ax.xaxis.set_major_formatter(years_fmt)
+        ax.xaxis.set_minor_locator(mdates.MonthLocator(interval=3))
+        ax.xaxis.set_minor_formatter(mon_fmt)
+
+        ## Y Axis format
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(y_tick_num))
+        
+      
+        string_format = '{x:,.0f}'
+        
+        ax.yaxis.set_major_formatter(ticker.StrMethodFormatter(string_format))
+
+        # Sets the limits of the x-axis: round to nearest years.
+        datemin = np.datetime64(data.index.min(), 'Y')
+        datemax = np.datetime64(data.index[-1], 'Y') + np.timedelta64(1, 'Y')
+        ax.set_xlim(datemin, datemax)
+
+        # Dynamically sets a bunch of labels
+        ax.set_title(title)
+        ax.set_xlabel(xlab)
+        ax.set_ylabel(ylab)
+        ax.legend()
+
+        # Specifies I want the grid for BOTH level of tick marks
+        ax.grid(True, which="both")
+
+        # rotates and right aligns the x labels, and moves the bottom of the
+        # axes up to make room for them
+        fig.autofmt_xdate(which='both')
+
+        path = './images/'+file_name+'.png'
+        plt.savefig(path)
+        plt.close(fig)  
+        pass
